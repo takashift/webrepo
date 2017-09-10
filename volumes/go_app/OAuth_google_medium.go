1001373c8d80ba1b0a5ace98db60d41e515c0335
takashift <e145771@ie.u-ryukyu.ac.jp>
2017-09-10 19:46:53 +0900

    サインイン方法選択画面の表示崩れと下線が表示されるのを修正

--- volumes/go_app/OAuth_google_medium.go
+++ volumes/go_app/OAuth_google_medium.go
@@ -8,7 +8,7 @@ import (
 )
 
 var googleOauthConfig = &oauth2.Config{
-	RedirectURL: "/",
+	RedirectURL: "https://webrepo.nal.ie.u-ryukyu.ac.jp/",
 	ClientID: "370442566774-osi0bgsn710brv1v3uc1s7hk24blhdq2.apps.googleusercontent.com",
 	ClientSecret: "E46tGSdcop7sU9L8pF30Nz_u",
 	Scopes: []string{
