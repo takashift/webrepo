1ebcaacf087215444108328a3a3e0e5a3529d8e7
takashift <e145771@ie.u-ryukyu.ac.jp>
2017-12-08 20:06:22 +0900

    ツイートを取得するjavaScriptコードをトップページに記述（まだエラー）

--- volumes/go_app/views/JWT.js
+++ volumes/go_app/views/JWT.js
@@ -1,9 +1,9 @@
 use strict;
 // var xhr = new XMLHttpRequest();
 
-xhr.setRequestHeader("Authorization", localStorage.jwt)
+xhr.setRequestHeader("Authorization", localStorage.jwt);
 
-localStorage.jwt = xhr.getResponseHeader("Authorization")
+localStorage.jwt = xhr.getResponseHeader("Authorization");
 
 function getHeader(){
 	$.ajax({
@@ -20,3 +20,21 @@ onload = function() {
 }
 
 document.getElementById("review_list").insertAdjacentHTML('afterbegin', document.createTextNode({{.Content}}));
+
+
+var xhr;
+onload = function () {
+  xhr = new XMLHttpRequest();
+  xhr.open("Get", "https://api.twitter.com/1.1/search/tweets.json?q=%23本日の評価ページお題&result_type=recent&count=1", false);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAADEp3gAAAAAA1utz8ysPQJLfahTh%2BIievBRebMw%3DJcaj7Z0fj0edHPVOZADriheIMugHxR1VsyZUod0y2eNQHHhbUS');
+  xhr.onreadystatechange = callback;
+  xhr.send(null);
+}
+function callback() {
+  if (xhr.status == 200) {
+	var myHeader = document.getElementById('daily_theme');
+	myHeader.value = xhr.responseText;
+	var tNode = document.createTextNode(xhr.responseText);
+	myHeader.appendChild(tNode);
+  }
+}
